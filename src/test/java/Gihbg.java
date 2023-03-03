f7b976ca0e4890b231e01e62d8063fd2e22f7235
Sandhyashree <msandya.sugu@gmail.com>
2023-03-03 10:41:15 +0530

    Hi Git

---
+++ src/test/java/Gihbg.java
@@ -0,0 +1,14 @@
+
+public class Gihbg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="Sai";
+
+boolean i = s.contains(s);
+
+System.out.println(i);
+
+	}
+
+}
